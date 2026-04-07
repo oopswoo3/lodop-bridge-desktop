fae267650b276c9d6f77ce67a8955d0c7331d3ef
oopswoo <im5@qq.com>
2026-04-07 09:59:45 +0800

    chore: 同步产品命名与开发环境配置

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   },
   // Tauri expects a fixed port, fail if that port is not available
   server: {
+    host: '127.0.0.1',
     port: 1420,
     strictPort: true,
   },
